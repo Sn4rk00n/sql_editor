0ac757f7f52ef0051f9cf4a1020d656bc5ce0f54
Unknown <arayni.max@gmail.com>
2018-03-14 16:52:50 +0100

    Init project

---
+++ main.h
@@ -0,0 +1,21 @@
+//---------------------------------------------------------------------------
+
+#ifndef mainH
+#define mainH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// Von der IDE verwaltete Komponenten
+private:	// Benutzer-Deklarationen
+public:		// Benutzer-Deklarationen
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
